7d27eaedc87892c5b2e08e0aab772ae9ba096462
pvikaz <152951615+pvikaz@users.noreply.github.com>
2024-04-29 23:28:51 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -179,8 +179,8 @@ class WZMLStyle:
     ETA =                                                ' | ⏰ <b>ETA:</b> {Eta}'
     SPEED =             '\n🚀 <b>Speed:</b> {Speed}'
     ELAPSED =                                     ' | ⏳ <b>TT:</b> {Elapsed}'
-    ENGINE =            '\n⚡ <b>Engine:</b> {Engine}'
-    STA_MODE =          '\n♨️ <b>Mode:</b> {Mode}'
+    ENGINE =            '\n♨️ <b>Engine:</b> {Engine}'
+    STA_MODE =          '\n⚡ <b>Mode:</b> {Mode}'
     SEEDERS =           '\n🌱 <b>Seeders:</b> {Seeders} | '
     LEECHERS =                                           '<b>Leechers:</b> {Leechers} 🦠'
 
