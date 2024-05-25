58e6d07f3b6d96018a266d6ca67641389daa78ac
pvikaz <152951615+pvikaz@users.noreply.github.com>
2024-05-25 17:56:59 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -3,9 +3,9 @@ class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
     ST_BN1_NAME = '🏴‍☠️ Repo 🏴‍☠️'
-    ST_BN1_URL = 'https://t.me/Mike_Tyson_Mirror_Leech'
+    ST_BN1_URL = 'https://t.me/KRATOS_MIKE_TYSON'
     ST_BN2_NAME = '☠ Updates ☠'
-    ST_BN2_URL = 'https://t.me/Mike_Tyson_Mirror_Leech'
+    ST_BN2_URL = 'https://t.me/KRATOS_MIKE_TYSON'
     ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
 <b>Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>📥 Now, This bot will send all your files and links here. Start Using ...</i>'''
